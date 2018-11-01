6d6106e871df95bae2486fd2dadc28ca2e11b86e
Marcos Rubio Sánchez <marcosrubio@outlook.com>
2018-11-01 19:52:08 +0100

    Add basic entities to AlohaContext

--- Model/Contexts/AlohaContext.cs
+++ Model/Contexts/AlohaContext.cs
@@ -1,11 +1,18 @@
+using Aloha.Model.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Aloha.Models.Contexts
 {
     public class AlohaContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Worker> Workers { get; set; }
+        public DbSet<Workstation> Workstations { get; set; }
+        public DbSet<Floor> Floors { get; set; }
+
         public AlohaContext(DbContextOptions<AlohaContext> options) : base(options)
         {
+            
         }
     }
 }
