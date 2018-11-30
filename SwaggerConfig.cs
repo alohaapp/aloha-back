fb43f5f2d09e926284a67311e5aa70edc2190af9
Marcos Rubio Sánchez <marcosrubio@outlook.com>
2018-11-30 23:02:31 +0100

    Alojify swagger

--- SwaggerConfig.cs
+++ SwaggerConfig.cs
@@ -2,13 +2,13 @@ namespace Aloha
 {
     public static class SwaggerConfig
     {
-        public const string EndpointDescription = "Aloha API v1";
+        public const string EndpointDescription = "Aloha 👋 API v1";
         public const string EndpointUrl = "/swagger/v1/swagger.json";
         public const string ContactName = "";
         public const string ContactUrl = "";
         public const string DocNameV1 = "v1";
         public const string DocInfoTitle = "Aloha API";
         public const string DocInfoVersion = "v1";
-        public const string DocInfoDescription = "The wonderous Aloha API";
+        public const string DocInfoDescription = "🌴 The wonderous Aloha API 🏝";
     }
 }
