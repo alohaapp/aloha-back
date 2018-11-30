76ec82a643e18f4f120aa63fff457583742667bb
Marcos Rubio Sánchez <marcosrubio@outlook.com>
2018-11-30 20:30:26 +0100

    Add FileHelper

---
+++ Services/FileHelper.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Net.Mime;
+using Aloha.Model.Entities;
+
+namespace Aloha.Helpers.FileHelper
+{
+    public class FileHelper
+    {
+        public static File GetFileFromBase64(string data)
+        {
+            try
+            {
+                string[] typeAndDataSplit = data.Split(";");
+
+                var contentType = new ContentType(typeAndDataSplit[0].Replace("data:", ""));
+                if (contentType == null) return null;
+
+                var dataBytes = Convert.FromBase64String(typeAndDataSplit[1].Split(",")[1]);
+                if (dataBytes.Length == 0) return null;
+
+                return new File
+                {
+                    MediaType = contentType.MediaType,
+                    Data = dataBytes
+                };
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
